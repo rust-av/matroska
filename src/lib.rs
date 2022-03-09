4d0e73c8e5d8ac3e215442a560d22a6dbfd427b9
Luni-4 <luni-4@hotmail.it>
2022-03-09 10:15:22 +0100

    Clean up crates definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,3 @@
-#![recursion_limit = "256"]
-#![allow(clippy::unreadable_literal)]
-// TODO: avoid these or replace
-//#![allow(clippy::large_enum_variant)]
-//
-#[macro_use]
-extern crate cookie_factory;
-
-extern crate av_data;
-extern crate av_format;
-
-#[cfg(test)]
-#[macro_use]
-extern crate quickcheck;
-
-// use av_data::rational;
-
 pub mod ebml;
 pub mod elements;
 pub mod permutation;
@@ -22,9 +5,3 @@ pub mod permutation;
 pub mod serializer;
 pub mod demuxer;
 pub mod muxer;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
-}
