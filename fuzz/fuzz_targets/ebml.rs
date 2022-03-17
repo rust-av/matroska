fe34d99c5feeea849b26cfcf080cb9c20d9345cf
Luni-4 <luni-4@hotmail.it>
2022-03-17 08:52:54 +0100

    Update fuzzer

--- fuzz/fuzz_targets/ebml.rs
+++ fuzz/fuzz_targets/ebml.rs
@@ -1,9 +1,8 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate matroska;
 
-use matroska::ebml::parse_element;
+use libfuzzer_sys::fuzz_target;
+use matroska::ebml::ebml_header;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = parse_element(data);
+    let _ = ebml_header(data);
 });
